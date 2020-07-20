28fe61ea0cc0f41ac53be3bc0dc660280773ccdc
Erfan Abdi <erfangplus@gmail.com>
2020-07-20 18:07:34 +0430

    input: touchscreen: Enable regulator by default
    * We’re not enabling dt2w from events so gesture_switch functions never getting called to enable or disable regulator, and having it always enabled doesn’t hurt.

--- drivers/input/touchscreen/ft8719_spi_c3j/focaltech_core.c
+++ drivers/input/touchscreen/ft8719_spi_c3j/focaltech_core.c
@@ -1534,7 +1534,7 @@ static int fts_ts_probe_entry(struct spi_device *client, struct fts_ts_data *ts_
 		goto err_get_regulator;
 	}
 
-	ret = fts_ts_enable_regulator(false);//default disable regulator
+	ret = fts_ts_enable_regulator(true);
 	if (ret < 0) {
 		FTS_ERROR("Failed to enable regulator");
 		goto err_enable_regulator;
